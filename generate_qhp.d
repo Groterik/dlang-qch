69b73e6e560cca725e8b6a8bb3ff8a4f6ce92f88
Cleem <cleemmi@gmail.com>
2014-08-18 23:09:12 +0400

    Add QHP generator

--- generate_qhp.d
+++ generate_qhp.d
@@ -40,28 +40,78 @@ struct Keyword
 
 auto parseKeywords(File f, const string fileName, const string section)
 {
-    auto regexp = regex(`<a name="(.*)"><\/a>`);
+    auto regexp = regex(`<a name="([^"]*)"><\/a>`);
     Keyword[] result;
     foreach(line; f.byLine())
     {
         auto m = match(line, regexp);
         if (m) {
-            auto str = to!string(m.captures[1]);
-            auto fullStr = section ~ "." ~ str;
-            auto link = fileName ~ "#" ~ str;
-            result ~= Keyword(str, fullStr, link);
+            auto name = to!string(m.captures[1]);
+            auto id1 = "D/" ~ section ~ "." ~ name;
+            auto id2 = "D/" ~ name;
+            auto link = fileName ~ "#" ~ name;
+            result ~= Keyword(name, id1, link);
+            result ~= Keyword(name, id2, link);
         }
     }
     return result;
+}
+
+struct Section
+{
+    string name;
+    string link;
+    this(const string n, const string l)
+    {
+        name = n;
+        link = l;
+    }
+}
+
+void generateQhp(File f, const string vers, const string[] files, const Section[] sections, const Keyword[] keywords)
+{
+    f.writeln(`<?xml version="1.0" encoding="UTF-8"?>
+    <QtHelpProject version="1.0">
+    <namespace>dlang.org.phobos.` ~ vers ~ `</namespace>
+      <virtualFolder>phobos</virtualFolder>
+      <customFilter name="Dlang Reference">
+        <filterAttribute>phobos</filterAttribute>
+        <filterAttribute>` ~ vers ~ `</filterAttribute>
+      </customFilter>
+      <filterSection>
+        <filterAttribute>phobos</filterAttribute>
+        <filterAttribute>` ~ vers ~ `</filterAttribute>
+        <toc>
+          <section title="Dlang Index" ref="index.html">`);
+    foreach(s; sections)
+    {
+        f.writeln(`          <section title="` ~ s.name ~ `" ref="` ~ s.link ~ `"/>`);
+    }
+    f.writeln(`          </section>`);
+    f.writeln(`        </toc>`);
+    f.writeln(`        <keywords>`);
+    foreach(k; keywords)
+    {
+        f.writeln(`         <keyword name="` ~ k.name ~ `" id="` ~ k.id ~ `" ref="` ~ k.link ~ `"/>`);
+    }
+    f.writeln(`        </keywords>`);
+    f.writeln(`        <files>`);
+    foreach(file; files)
+    {
+        f.writeln(`          <file>` ~ file ~ `</file>`);
+    }
+    f.writeln(`        </files>`);
+    f.writeln(`      </filterSection>`);
+    f.writeln(`    </QtHelpProject>`);
 }
 
 int main(string[] args)
 {
     string htmlDirectory;
     string htmlPattern;
-    string qhpFilename;
-    getopt(args, "input|i", &htmlDirectory, "output|o", &qhpFilename, "pattern|p", &htmlPattern);
-    if (htmlDirectory.length == 0 || qhpFilename.length == 0)
+    string qhpFileName;
+    getopt(args, "input|i", &htmlDirectory, "output|o", &qhpFileName, "pattern|p", &htmlPattern);
+    if (htmlDirectory.length == 0 || qhpFileName.length == 0)
     {
         usage(args[0]);
         return 1;
@@ -72,7 +122,7 @@ int main(string[] args)
     }
 
     string[] fileNames;
-    string[] sections;
+    Section[] sections;
     Keyword[] keywords;
 
     auto inFiles = dirEntries(htmlDirectory, htmlPattern, SpanMode.depth);
@@ -85,9 +135,10 @@ int main(string[] args)
             fileNames ~= f.name();
             auto section = parseSection(fi);
             keywords ~= parseKeywords(fi, f.name(), section);
-            sections ~= section;
+            sections ~= Section(section, f.name());
         }
     }
-    writeln(sections);
+    auto qhp = File(qhpFileName, "w");
+    generateQhp(qhp, "2.065", fileNames, sections, keywords);
     return 0;
 }
